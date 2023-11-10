efc6886b6a024be14e3046075b42be88fb084184
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-10 19:52:18 +0100

    sent message with email.js

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ const projects = [
 
 // selecting elements
 
-
 const projectContainer = document.querySelector('.project__container');
 const categoryBtns = document.querySelectorAll('.category__btn');
 
@@ -35,7 +34,6 @@ const categoryBtns = document.querySelectorAll('.category__btn');
 window.addEventListener('DOMContentLoaded', function () {
   displayProjects(projects);
 });
-
 
 // display Projects
 function displayProjects(projectItems) {
@@ -94,3 +92,53 @@ categoryBtns.forEach((btn) => {
     displayProjects(filteredProjects);
   });
 });
+
+// EMAIl JS
+
+const contactForm = document.getElementById('contact-form'),
+  contactName = document.getElementById('contact-name'),
+  contactEmail = document.getElementById('contact-email'),
+  Message = document.getElementById('message'),
+  contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  if (
+    contactName.value === '' ||
+    contactEmail.value === '' ||
+    Message.value === ''
+  ) {
+    //and and remove color
+    contactMessage.classList.remove('color-light');
+    contactMessage.classList.add('color-dark');
+    //show message
+    contactMessage.textContent = 'Write all the input fields';
+  } else {
+    //serviceID - template
+    emailjs
+      .sendForm(
+        'service_6rwkr0h',
+        'template_iqlesir',
+        '#contact-form',
+        'Tvbl6CRVtPn6hqi4Y'
+      )
+      .then(
+        () => {
+          //show message and add color,window + dot to open emojii
+          contactMessage.classList.add('.color-light');
+          contactMessage.textContent = 'Message sent ✔️';
+          //remove message
+          setTimeout(() => {
+            contactMessage.textContent = '';
+          }, 5000);
+        },
+        (error) => {
+          alert('OOPs! Something went wrong...', error);
+        }
+      );
+      contactName.value = '';
+      contactEmail.value = '';
+      Message.value = '';
+  }
+};
+contactForm.addEventListener('submit', sendEmail);
