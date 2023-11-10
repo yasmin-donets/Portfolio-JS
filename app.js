efe79ff4a4091d3ceab696bb8f498c9fa3a002e5
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-10 20:15:06 +0100

    scroll header js and css

--- app.js
+++ app.js
@@ -136,9 +136,22 @@ const sendEmail = (e) => {
           alert('OOPs! Something went wrong...', error);
         }
       );
-      contactName.value = '';
-      contactEmail.value = '';
-      Message.value = '';
+    contactName.value = '';
+    contactEmail.value = '';
+    Message.value = '';
   }
 };
 contactForm.addEventListener('submit', sendEmail);
+
+// Background header
+
+function scrollHeader() {
+  const header = document.getElementById('header');
+  if (this.scrollY >= 50) {
+    header.classList.add('scroll-header');
+  } else {
+    header.classList.remove('scroll-header');
+  }
+}
+
+window.addEventListener('scroll', scrollHeader);
