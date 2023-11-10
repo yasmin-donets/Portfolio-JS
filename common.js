aa6ec4fa8ab229cc47b4e55b9d7ea48bed4a44d0
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-11 00:35:33 +0100

    common js nav menu toggle / close / remove menu

--- common.js
+++ common.js
@@ -1,3 +1,27 @@
+// Show Menu
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+// menu show
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+// hide menu show
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+// remove menu in mobile vers
+const navLinks = document.querySelectorAll('.nav__link');
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
+}
+navLinks.forEach((link) => link.addEventListener('click', linkAction));
 // scrollHeader
 
 function scrollHeader() {
