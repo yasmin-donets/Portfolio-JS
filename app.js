c39d4ef035d1253ceaece22ec8e1e6c39cbce22c
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-22 21:07:09 +0100

    changes

--- app.js
+++ app.js
@@ -1,29 +1,4 @@
-// data
-const projects = [
-  {
-    id: 1,
-    title: 'web-sites',
-    category: 'web-sites',
-    img: './images/item-1.jpeg',
-    desc: `My project is a user-friendly web application that provides real-time weather information for any desired location. With seamless integration of OpenWeatherMap API. An intuitive interface ensures a smooth user experience, making staying informed about weather conditions a hassle-free task. Built With : HTML & CSS Javascript OpenWeatherMap API`,
-  },
-  {
-    id: 2,
-    img: './images/project-1.png',
-    title: 'web-apps',
-    category: 'web-apps',
-    img: './images/item-1.jpeg',
-    desc: `My project is a user-friendly web application that provides real-time weather information for any desired location. With seamless integration of OpenWeatherMap API. An intuitive interface ensures a smooth user experience, making staying informed about weather conditions a hassle-free task. Built With : HTML & CSS Javascript OpenWeatherMap API`,
-  },
-  {
-    id: 3,
-    img: './images/project-1.png',
-    title: 'landing',
-    category: 'landing',
-    img: './images/item-1.jpeg',
-    desc: `My project is a user-friendly web application that provides real-time weather information for any desired location. With seamless integration of OpenWeatherMap API. An intuitive interface ensures a smooth user experience, making staying informed about weather conditions a hassle-free task. Built With : HTML & CSS Javascript OpenWeatherMap API`,
-  },
-];
+import projects from './utils/data.js';
 
 // selecting elements
 
@@ -34,7 +9,6 @@ const categoryBtns = document.querySelectorAll('.category__btn');
 window.addEventListener('DOMContentLoaded', function () {
   displayProjects(projects);
 });
-
 
 // display Projects
 function displayProjects(projectItems) {
@@ -93,7 +67,3 @@ categoryBtns.forEach((btn) => {
     displayProjects(filteredProjects);
   });
 });
-
-
-
-
---
+++ utils/data.js
@@ -0,0 +1,27 @@
+// data
+const projects = [
+  {
+    id: 1,
+    title: 'web-sites',
+    category: 'web-sites',
+    img: './images/item-1.jpeg',
+    desc: `My project is a user-friendly web application that provides real-time weather information for any desired location. With seamless integration of OpenWeatherMap API. An intuitive interface ensures a smooth user experience, making staying informed about weather conditions a hassle-free task. Built With : HTML & CSS Javascript OpenWeatherMap API`,
+  },
+  {
+    id: 2,
+    img: './images/project-1.png',
+    title: 'web-apps',
+    category: 'web-apps',
+    img: './images/item-1.jpeg',
+    desc: `My project is a user-friendly web application that provides real-time weather information for any desired location. With seamless integration of OpenWeatherMap API. An intuitive interface ensures a smooth user experience, making staying informed about weather conditions a hassle-free task. Built With : HTML & CSS Javascript OpenWeatherMap API`,
+  },
+  {
+    id: 3,
+    img: './images/project-1.png',
+    title: 'landing',
+    category: 'landing',
+    img: './images/item-1.jpeg',
+    desc: `My project is a user-friendly web application that provides real-time weather information for any desired location. With seamless integration of OpenWeatherMap API. An intuitive interface ensures a smooth user experience, making staying informed about weather conditions a hassle-free task. Built With : HTML & CSS Javascript OpenWeatherMap API`,
+  },
+];
+export default projects;
